214dfc5dfff7242968cae5080713cf393c7fc6e9
redn1nja <ostap.seryvko@ucu.edu.ua>
2024-04-10 17:44:22 +0300

    got odom right

--- src/turtleSLAM/build.sh
+++ src/turtleSLAM/build.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 colcon build --symlink-install --packages-select turtleSLAM
-source install/local_setup.bash
+. install/local_setup.bash
--- src/turtleSLAM/turtleSLAM/parse_odom.py
+++ src/turtleSLAM/turtleSLAM/parse_odom.py
@@ -1,5 +1,34 @@
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from rclpy import qos
+
+class ParseOdom(Node):
+    def __init__(self):
+        # qos_profile = QoSProfile(
+        #     reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+        #     history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+        #     )
+        super().__init__('parse_odom')
+        self.subscription = self.create_subscription(
+            Odometry,
+            'odom',
+            self.odom_callback,
+            qos_profile=qos.qos_profile_sensor_data)
+        self.subscription 
+    
+    def odom_callback(self, msg):
+        self.get_logger().info('Received odometry message')
+        self.get_logger().info('Position: x=%f, y=%f, z=%f' % (msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z))
+        self.get_logger().info('Orientation: x=%f, y=%f, z=%f, w=%f' % (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w))
+
+
 def main():
-    print('Hi from turtleSLAM.')
+    rclpy.init()
+    parse_odom = ParseOdom()
+    rclpy.spin(parse_odom)
+    parse_odom.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
