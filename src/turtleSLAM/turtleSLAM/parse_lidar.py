f62f723f3c6e5bb83e1d9916d66ece4d72e975e0
beheni <anastasiia.beheni@ucu.edu.ua>
2024-04-21 22:02:46 +0300

    added visuals + rosbag with scan data for tests

--- src/turtleSLAM/turtleSLAM/parse_lidar.py
+++ src/turtleSLAM/turtleSLAM/parse_lidar.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+import matplotlib.pyplot as plt
+import numpy as np
 from rclpy import qos
 
 class ParseLidar(Node):
@@ -11,11 +13,25 @@ class ParseLidar(Node):
             'scan',
             self.lidar_callback,
             qos_profile=qos.qos_profile_sensor_data)
-        self.subscription 
+        self.fig, self.ax = plt.subplots(subplot_kw={'projection': 'polar'})
+        plt.ion()  # Enable interactive mode for continuous updating
+        plt.show()
+
     
-    def lidar_callback(self, msg):
+    def lidar_callback(self, scan_msg):
+        angles = np.arange(scan_msg.angle_min, scan_msg.angle_max, scan_msg.angle_increment)
+        ranges = np.array(scan_msg.ranges)
+        valid_ranges = np.logical_and(ranges > scan_msg.range_min, ranges < scan_msg.range_max)
+        valid_angles = angles[valid_ranges]
+        valid_ranges = ranges[valid_ranges]
+        self.ax.clear()
+        self.ax.scatter(valid_angles, valid_ranges, s=1)
+        self.ax.set_theta_zero_location('N')
+        self.ax.set_title('Laser Scan Data')
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
         self.get_logger().info('Received lidar data')
-        self.get_logger().info(f'Ranges: x={msg.ranges}')
+        # self.get_logger().info(f'Ranges: x={scan_msg.ranges}')
 
 def main():
     rclpy.init()
